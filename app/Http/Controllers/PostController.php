2724962f1749bb12f3ec3219a7f24d86ab5ded36
Ahmed Ashraf <119435375+AhmedAshraf711@users.noreply.github.com>
2026-07-14 05:07:06 +0300

    Add Form Request validation for store and update requests

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Http\Requests\StorePostRequest;
+use App\Http\Requests\UpdatePostRequest;
 use App\Models\User;
 use App\Models\Post;
 use App\Repositories\Interfaces\PostRepositoryInterface;
@@ -14,48 +16,30 @@ class PostController extends Controller
         $this->postRepository = $postRepository;
     }
    
-    public function index(){
-        //$allposts= Post::with('user')->get();
-        $allposts= $this->postRepository->getAll();
-        return response()->json($allposts,200);
+    public function index()
+    {
+         $allposts= $this->postRepository->getAll();
+         return response()->json($allposts,200);
     }
-    public function show($id){
-        //$singlePost=Post::with('user')->findOrFail($id);
-        $singlePost=$this->postRepository->getById($id);
-        return response()->json($singlePost,200); 
+    public function show($id)
+    {
+         $singlePost=$this->postRepository->getById($id);
+         return response()->json($singlePost,200); 
     }
-    public function store(Request $request){
-            // $newpost = new Post;
-            // $newpost->title = $request->title;
-            // $newpost->description = $request->description;
-            // $newpost->user_id = $request->user_id;
-            // $newpost->save();
-            $this->postRepository->create([
-                'title'=> $request->title,
-                'description'=> $request->description,
-                'user_id'=> $request->user_id
-            ]);
+    public function store(StorePostRequest $validRequest)
+    {
+         $this->postRepository->create($validRequest->validated());
          return response()->json(['message'=>'post added successfully'],201);
     }
-    public function update(Request $request, $id){
-        // $post = Post::findOrFail($id);
-        // $post->title = $request->title;
-        // $post->description = $request->description;
-        // $post->user_id= $request->user_id;
-        // $post->save();
-
-        $this->postRepository->update($id,[
-          'title'=> $request->title,
-          'description'=> $request->description,
-          'user_id'=> $request->user_id
-        ]);
+    public function update(UpdatePostRequest $validRequest, $id)
+    {
+         $this->postRepository->update($id, $validRequest->validated());
          return response()->json(['message'=>'post updated successfully'],201);
     }
     public function destroy($id)
     {
-     
-           $this->postRepository->delete($id);
-           return response()->json(['message'=> 'post deleted successfully'],201);
+         $this->postRepository->delete($id);
+         return response()->json(['message'=> 'post deleted successfully'],201);
     }
         
 }
